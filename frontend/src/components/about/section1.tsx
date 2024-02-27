c9e558e7dfe8090cf974d50446445bb3c7a1b39f
Mario1870 <marioraach01@gmail.com>
2024-02-27 17:34:34 +0100

    changed text generation of about page

--- frontend/src/components/about/section1.tsx
+++ frontend/src/components/about/section1.tsx
@@ -1,15 +1,17 @@
 import Typewriter from '../../animations/typewriter';
 import SlideUpWhenVisible from '../../animations/slideUpWhenVisible';
+import { TextGenerateEffect } from '../../ui_components/aceternity/text-generate-effect';
 
 const About_Section1 = () => {
+
+    const words = "Your brand is what people say about you when you're not in the room."
 
     return (
         <>
             <section className='min-w-screen min-h-svh grid xl:grid-cols-2 ' >
-                <div className='sm:min-h-screen px-10 2xl:px-40 text-gray-500 text-3xl sm:text-7xl bg-backgroundGray rounded-br-3xl rounded-bl-3xl md:rounded-bl-none grid grid-rows-[3fr_3fr_3fr] md:mr-2 md:mb-2 order-last md:order-first h-72 pt-4'>
-                    <span className='hidden sm:block font-roboto'></span>
-                    <Typewriter speed={80}>"Your brand is what people say about you when you're not in the room."</Typewriter>
-                    <div className='text-xl w-full text-end'><SlideUpWhenVisible delay={6.6} y={20}>Jeff Bezos</SlideUpWhenVisible></div>
+                <div className='sm:min-h-screen px-10 2xl:px-20 2xl:pl-40 rounded-br-3xl flex flex-col justify-center items-center'>
+                    <TextGenerateEffect words={"\"" + words + "\""} className='text-gray-500 text-3xl sm:text-7xl font-roboto' />
+                    <div className='text-2xl w-full text-end text-gray-600'><SlideUpWhenVisible delay={3.6} y={20}>Jeff Bezos</SlideUpWhenVisible></div>
                 </div>
                 <div className='p-10 order-first md:order-none sm:hidden flex justify-center xl:items-center xl:flex'>
                     <span className='w-full max-w-80 xl:max-w-[36rem] max-h-80 xl:max-h-[36rem] rounded-full shadow-lg shadow-white mt-20 md:mt-0 grayscale bg-hemd_small bg-center bg-cover'>
--- frontend/src/components/about/section5.tsx
+++ frontend/src/components/about/section5.tsx
@@ -7,7 +7,6 @@ import { SiSqlite } from "react-icons/si";
 import SlideUpWhenVisible from '../../animations/slideUpWhenVisible';
 import { InfiniteMovingCards } from '../../ui_components/aceternity/infinite-moving-cars';
 const Technik = lazy(() => import("./technik"));
-const LogoSliderReactIcons = lazy(() => import("../shared/logoSliderReactIcons"));
 
 const size = 50
 const color = "white"
@@ -27,38 +26,6 @@ const icons = [
     <SiSqlite size={size} color={color} aria-hidden="true" />
 ];
 
-const testimonials = [
-    {
-      quote:
-        "It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair.",
-      name: "Charles Dickens",
-      title: "A Tale of Two Cities",
-    },
-    {
-      quote:
-        "To be, or not to be, that is the question: Whether 'tis nobler in the mind to suffer The slings and arrows of outrageous fortune, Or to take Arms against a Sea of troubles, And by opposing end them: to die, to sleep.",
-      name: "William Shakespeare",
-      title: "Hamlet",
-    },
-    {
-      quote: "All that we see or seem is but a dream within a dream.",
-      name: "Edgar Allan Poe",
-      title: "A Dream Within a Dream",
-    },
-    {
-      quote:
-        "It is a truth universally acknowledged, that a single man in possession of a good fortune, must be in want of a wife.",
-      name: "Jane Austen",
-      title: "Pride and Prejudice",
-    },
-    {
-      quote:
-        "Call me Ishmael. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world.",
-      name: "Herman Melville",
-      title: "Moby-Dick",
-    },
-  ];
-
 const About_Section5 = () => {
 
     return (
@@ -69,11 +36,11 @@ const About_Section5 = () => {
                 </Suspense>     
                 <SlideUpWhenVisible>
                     <Suspense fallback={<div>Lädt...</div>}>
-                    <InfiniteMovingCards
-                        items={testimonials}
-                        direction="right"
-                        speed="slow"
-                    />
+                        <InfiniteMovingCards
+                            items={icons}
+                            direction="right"
+                            speed="slow"
+                        />
                     </Suspense>
                 </SlideUpWhenVisible>              
             </section>
