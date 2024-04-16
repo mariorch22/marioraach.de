939640b3fb98deb465d6da9e10d467bec165323b
Mario1870 <marioraach01@gmail.com>
2024-04-16 21:53:10 +0200

    updated languageselector to useToggle

--- frontend/src/components/home/language-selector.tsx
+++ frontend/src/components/home/language-selector.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useCallback } from "react";
+import { useCallback } from "react";
 import { useTranslation } from 'react-i18next';
 import { IoLanguageOutline } from "react-icons/io5";
 import { motion } from "framer-motion";
+import useToggle from "../../hooks/useToggle";
 
 const variantsDE = {
     hidden: {
@@ -49,46 +50,40 @@ const variantsEN = {
   };
 
 const LanguageSelector = () => {
-    const [isClicked, setIsClicked] = useState(false);
+    const [isClicked, setIsClicked] = useToggle(false);
     const { i18n } = useTranslation();
 
     const changeLanguage = useCallback((lng:string) => {
         i18n.changeLanguage(lng);
-        setIsClicked(false); // Schließt das Menü nach der Auswahl
+        setIsClicked(); // Schließt das Menü nach der Auswahl
     }, [i18n]);
-
-    const handleClick = useCallback(() => {
-        setIsClicked(current => !current);
-    }, []);
 
     return (
         <div className="mb-12 mr-8 flex w-40 h-40 relative">
-            <IoLanguageOutline
-                className="w-12 h-12 cursor-pointer absolute right-0 bottom-0 z-50"
-                onClick={handleClick}
+          <IoLanguageOutline
+            className="w-12 h-12 cursor-pointer absolute right-0 bottom-0 z-50"
+            onClick={setIsClicked}
+          />
+
+          <div className="absolute w-40 h-40">
+            <motion.span
+              className={`absolute bottom-0 right-0 w-12 h-12 bg-backgroundGray rounded-full p-2 pr-4 inline-block bg-cover z-20 cursor-pointer ${i18n.language === 'de' ? 'border-2 border-green-500' : ''}`}
+              style={{ backgroundImage: `url(/images/deutsch.webp)` }}
+              onClick={() => changeLanguage('de')}
+              variants={variantsDE}
+              initial="hidden"
+              animate={isClicked ? "visible" : "hidden"}
             />
 
-
-                <div className="absolute w-40 h-40">
-                    <motion.span
-                        className={`absolute bottom-0 right-0 w-12 h-12 bg-backgroundGray rounded-full p-2 pr-4 inline-block bg-cover z-20 cursor-pointer ${i18n.language === 'de' ? 'border-2 border-green-500' : ''}`}
-                        style={{ backgroundImage: `url(/images/deutsch.webp)` }}
-                        onClick={() => changeLanguage('de')}
-                        variants={variantsDE}
-                        initial="hidden"
-                        animate={isClicked ? "visible" : "hidden"}
-                    />
-
-                    <motion.span
-                        className={`absolute bottom-0 right-0 w-12 h-12 bg-backgroundGray rounded-full p-2 pr-4 inline-block bg-cover z-20 cursor-pointer ${i18n.language === 'en' ? 'border-2 border-green-500' : ''}`}
-                        style={{ backgroundImage: `url(/images/englisch.webp)` }}
-                        onClick={() => changeLanguage('en')}
-                        variants={variantsEN}
-                        initial="hidden"
-                        animate={isClicked ? "visible" : "hidden"}
-                    />
-                </div>
-
+            <motion.span
+              className={`absolute bottom-0 right-0 w-12 h-12 bg-backgroundGray rounded-full p-2 pr-4 inline-block bg-cover z-20 cursor-pointer ${i18n.language === 'en' ? 'border-2 border-green-500' : ''}`}
+              style={{ backgroundImage: `url(/images/englisch.webp)` }}
+              onClick={() => changeLanguage('en')}
+              variants={variantsEN}
+              initial="hidden"
+              animate={isClicked ? "visible" : "hidden"}
+            />
+          </div>
         </div>
 
     );
