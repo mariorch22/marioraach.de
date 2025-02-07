09e6db7747efe97f7c3f4c5f3f00b25e949c6348
mario1870 <marioraach01@gmail.com>
2025-02-07 20:45:25 +0100

    fix: add internationalization support for footer links

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import { Link } from 'react-router-dom';
 import { CiMail } from 'react-icons/ci';
 import { FOOTER_LINKS, CONTACT_EMAIL } from '@/constants/footer';
 import { FooterLink } from './components/footerLink';
+import { useTranslation } from 'react-i18next';
 
 const Footer = () => {
+  const { i18n } = useTranslation();
+  
   return (
     <>
       <footer className="pt-8">
@@ -23,8 +25,8 @@ const Footer = () => {
             <div className="text-gray-30 text-center text-xs md:text-sm opacity-80 text-md flex flex-col justify-center">
               <p>&copy; {new Date().getFullYear()} - All rights reserved</p>
               <div className="flex gap-2 w-full justify-center">
-                <Link to={'/imprint'}>Impressum</Link>
-                <Link to={'/dataprotection'}>Datenschutz</Link>
+                <Link to={'/imprint'}>{i18n.language === 'de' ? 'Impressum' : "Imprint"}</Link>
+                <Link to={'/dataprotection'}>{i18n.language === 'de' ? 'Datenschutz' : "Data Protection"}</Link>
               </div>
             </div>
 
