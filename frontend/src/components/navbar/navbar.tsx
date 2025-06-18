a9d6d677d21b78c23fbbc5b03cbb2f482e618ae9
mario1870 <marioraach01@gmail.com>
2025-06-18 21:23:52 +0200

    Enhance LanguageSelector styling in Navbar component
    - Updated the LanguageSelector component in the Navbar to increase text size for better visibility.

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -23,7 +23,7 @@ const Navbar: FC = () => {
   return (
     <nav className="bg-black fixed z-40 flex h-20 w-full items-center justify-between px-4 py-2 drop-shadow-customDropShadow2 md:px-8">
       <Logo />
-      <LanguageSelector className="pr-4 opacity-70" />
+      <LanguageSelector className="pr-4 opacity-70 text-2xl" />
     </nav>
   );
 };
